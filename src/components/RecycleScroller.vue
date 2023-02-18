9140035da5139ed9175dd4685a9a1ca53fd04622
liudebug <937489268@qq.com>
2023-02-18 20:05:30 +0800

    Update RecycleScroller.vue

--- src/components/RecycleScroller.vue
+++ src/components/RecycleScroller.vue
@@ -26,7 +26,7 @@
       <div
         v-for="view of pool"
         :key="view.nr.id"
-        :style="ready ? { transform: `translate${direction === 'vertical' ? 'Y' : 'X'}(${view.position}px)` } : null"
+        :style="ready ? { transform: `translate${direction === 'vertical' ? 'Y' : 'X'}(${view.position}vw)` } : null"
         class="vue-recycle-scroller__item-view"
       >
         <slot
